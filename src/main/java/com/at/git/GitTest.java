f15ae8c6ed61260fb3deed72e45dd3d27062e88d
Lrs <Lrs@qq.com>
2022-07-18 18:05:19 +0800

    third commit

--- src/main/java/com/at/git/GitTest.java
+++ src/main/java/com/at/git/GitTest.java
@@ -3,6 +3,7 @@ package com.at.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git2");
+        System.out.println("hello git2！");
+        System.out.println("hello git3！");
     }
 }
